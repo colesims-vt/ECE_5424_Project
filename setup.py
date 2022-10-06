d6df821ffbb0cf19084ba58742a92a69a122829c
Cole Sims <sims@vt.edu>
2022-10-05 23:03:49 -0400

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Identify and solve a problem using machine learning techniques.',
+    author='Team P (Patel, Sims, Smith)',
+    license='MIT',
+)
